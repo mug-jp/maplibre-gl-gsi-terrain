5af24789cee1a5158929bc5673c5828bb663a228
Kanahiro <mediterranean1769@gmail.com>
2022-12-02 00:59:36 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 // vite.config.js
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     build: {
-        lib: {
-            entry: resolve(__dirname, 'src/terrain.ts'),
-            name: 'maplibre-gl-gsi-terrain',
-            fileName: (format) => `maplibre-gl-gsi-terrain.${format}.js`,
-        },
+        outDir: 'example',
     },
+    base: './',
 });
